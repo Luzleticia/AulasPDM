57524aba1f1c5145262bd5feee304401033b5fb6
lab <user@IFPR-Lab.localdomain>
2023-02-28 21:59:36 -0300

    Initial commit

---
+++ app/src/main/java/ifpr/aulapdm/Activity2Tela.java
@@ -0,0 +1,14 @@
+package ifpr.aulapdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2Tela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2_tela);
+    }
+}
